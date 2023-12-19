0f622fc9c35c7653a6240476dcb8cf89aa8cc3fa
Linda <2210859017@fh-burgenland.at>
2023-12-19 21:46:10 +0100

    feat: added sensor-dashboard component

---
+++ src/app/sensor-dashboard/sensor-dashboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sensor-dashboard',
+  templateUrl: './sensor-dashboard.component.html',
+  styleUrls: ['./sensor-dashboard.component.scss']
+})
+export class SensorDashboardComponent {
+
+}
