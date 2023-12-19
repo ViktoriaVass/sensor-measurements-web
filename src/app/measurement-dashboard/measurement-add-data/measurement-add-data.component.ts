d041e638462e0aed15762ff38a2ea658b8545157
Linda <2210859017@fh-burgenland.at>
2023-12-19 21:44:55 +0100

    feat: added measurement-add-data component

---
+++ src/app/measurement-dashboard/measurement-add-data/measurement-add-data.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-measurement-add-data',
+  templateUrl: './measurement-add-data.component.html',
+  styleUrls: ['./measurement-add-data.component.scss']
+})
+export class MeasurementAddDataComponent {
+
+}
