5f6a620b37018f4c4fbbe646d3c89cec26c35144
Linda <2210859017@fh-burgenland.at>
2023-12-19 21:46:54 +0100

    feat: added router outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SensorDashboardComponent} from "./sensor-dashboard/sensor-dashboard.component";
+import {MeasurementDashboardComponent} from "./measurement-dashboard/measurement-dashboard.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: SensorDashboardComponent},
+  { path: 'sensor', component: SensorDashboardComponent },
+  { path: 'measurement', component: MeasurementDashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SensorDashboardComponent } from './sensor-dashboard/sensor-dashboard.component';
+import { MeasurementDashboardComponent } from './measurement-dashboard/measurement-dashboard.component';
+import { HeaderComponent } from './header/header.component';
+import { SensorDataComponent } from './sensor-dashboard/sensor-data/sensor-data.component';
+import { SensorAddDataComponent } from './sensor-dashboard/sensor-add-data/sensor-add-data.component';
+import { MeasurementDiagramComponent } from './measurement-dashboard/measurement-diagram/measurement-diagram.component';
+import { MeasurementDataComponent } from './measurement-dashboard/measurement-data/measurement-data.component';
+import { MeasurementAddDataComponent } from './measurement-dashboard/measurement-add-data/measurement-add-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SensorDashboardComponent,
+    MeasurementDashboardComponent,
+    HeaderComponent,
+    SensorDataComponent,
+    SensorAddDataComponent,
+    MeasurementDiagramComponent,
+    MeasurementDataComponent,
+    MeasurementAddDataComponent
   ],
   imports: [
     BrowserModule,
