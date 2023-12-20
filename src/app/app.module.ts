19dafec4f4e0bd6f93fb0ed7dbf2a4eca6779d40
ViktoriaVass <2210859014@fh-burgenland.at>
2023-12-20 15:34:12 +0100

    sensorTable with http connection - not working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MeasurementAddDataComponent } from './measurement-dashboard/measurement
 import {MatTableModule} from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -34,6 +35,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatTableModule,
     MatSortModule,
     BrowserAnimationsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/backend.service.ts
+++ src/app/shared/backend.service.ts
@@ -1,52 +1,36 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {ISensor} from "./interfaces/Sensor";
-import {StoreService} from "./store.service";
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { StoreService } from './store.service';
+import { ISensor } from './interfaces/Sensor';
+import * as xml2js from 'xml2js';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class BackendService {
+  private apiUrl = 'http://localhost:8090/sensor'; 
 
-    constructor(public storeService: StoreService) { }
+  constructor(public storeService: StoreService, private http: HttpClient) {}
 
-    public getSensors(): Observable<ISensor[]> {
-      return of([
-        {
-            "name": "Kitchen",
-            "location": "kitchen",
-            "isActive": true,
-            "type": "indoor"
-        },
-      ]);
-    }
+  public getSensors(): Observable<ISensor[]> {
+    const fullUrl = this.apiUrl;
+    console.log('Full URL:', fullUrl);
+    return this.http.get(this.apiUrl, { responseType: 'text' }).pipe(
+      map((xmlResponse: string) => {
+        let jsonData: any;
+        xml2js.parseString(xmlResponse, { explicitArray: false }, (error, result) => {
+          if (error) {
+            throw new Error('Error parsing XML response: ' + error.message);
+          }
+          jsonData = result;
+        });
+
+        // Adjust this part based on the actual XML structure
+        const sensors = jsonData && jsonData.sensor ? jsonData.sensor : [];
+        return Array.isArray(sensors) ? sensors : [sensors];
+      })
+    );
   }
-    /*
-    public getSensors() {
-        this.http.get<Sensor[]>('http://localhost:8083/sensor').subscribe(data => {
-            this.storeService.sensors = data;
-        });
-    }
-
-    /*
-    public getChildren(page: number) {
-        this.http.get<ChildResponse[]>(`http://localhost:5000/childs?_expand=kindergarden&_page=${page}&_limit=${CHILDREN_PER_PAGE}`, { observe: 'response' }).subscribe(data => {
-            this.storeService.children = data.body!;
-            this.storeService.childrenTotalCount = Number(data.headers.get('X-Total-Count'));
-            this.storeService.isLoading = false;
-        });
-    }
-
-    public addChildData(child: Child, page:  number) {
-        this.http.post('http://localhost:5000/childs', child).subscribe(_ => {
-            this.getChildren(page);
-        })
-    }
-
-    public deleteChildData(childId: string, page: number) {
-        this.http.delete(`http://localhost:5000/childs/${childId}`).subscribe(_=> {
-            this.getChildren(page);
-        })
-    }
-     */
+}
